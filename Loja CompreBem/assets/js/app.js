5ff8e0c56b4da5c9c2e9c28a579078f9178fc9ee
airtonabrahao <airtonbomfacebook@gmail.com>
2023-12-06 11:38:15 -0300

    pagina de produtos atualizada

--- Loja CompreBem/assets/js/app.js
+++ Loja CompreBem/assets/js/app.js
@@ -9,3 +9,22 @@ function menucelular(){
         MenuItens.style.maxHeight = "0px";
     }
 }
+
+var produtoImg = document.getElementsById("produtosImg");
+var produtoMiniatura = document.getElementsByClassName("produtosMiniatura");
+
+produtoMiniatura[0].onclick = function(){
+    produtosImg.src = produtosMiniatura[0].src;
+}
+
+produtoMiniatura[1].onclick = function(){
+    produtosImg.src = produtosMiniatura[1].src;
+}
+
+produtoMiniatura[2].onclick = function(){
+    produtosImg.src = produtosMiniatura[2].src;
+}
+
+produtoMiniatura[3].onclick = function(){
+    produtosImg.src = produtosMiniatura[3].src;
+}
